6ba92643b9e3cae46efe98806ffcd47d5fc533c9
jhk01007 <jhk001007@naver.com>
2024-08-20 19:44:52 +0900

    refactor: BoardWriteServlet 리팩토링

--- src/main/java/org/example/day0819_board_project/controller/board/BoardWriteServlet.java
+++ src/main/java/org/example/day0819_board_project/controller/board/BoardWriteServlet.java
@@ -9,6 +9,7 @@ import jakarta.servlet.http.HttpSession;
 import org.example.day0819_board_project.constant.Alert;
 import org.example.day0819_board_project.constant.ViewsPath;
 import org.example.day0819_board_project.domain.Board;
+import org.example.day0819_board_project.exception.AuthenticatedException;
 import org.example.day0819_board_project.service.BoardService;
 
 import java.io.IOException;
@@ -24,17 +25,18 @@ public class BoardWriteServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        // 로그인 된 사용자인지 체크
-        HttpSession session = req.getSession();
-        String login = (String) session.getAttribute("login");
+        try {
+            // 로그인 된 사용자인지 체크
+            HttpSession session = req.getSession();
+            String login = (String) session.getAttribute("login");
 
-        //로그인된 사용자라면 글 작성 가능
-        if(login != null)
+            //로그인된 사용자라면 글 작성 가능
+            if (login == null)
+                throw new AuthenticatedException("글을 작성할 권한이 없습니다. 로그인을 해주세요.");
+
             req.getRequestDispatcher(ROOT + "board/write_form.jsp").forward(req, resp);
-        else {
-            req.setAttribute("msg", "접근할 수 없습니다. 글을 작성하려면 로그인을 해주세요");
-            req.setAttribute("path", req.getContextPath());
-            req.getRequestDispatcher(ROOT + "alert.jsp").forward(req, resp);
+        } catch (AuthenticatedException e) {
+            alertAndRedirect(req, e.getMessage(), req.getContextPath(), resp);
         }
 
     }
@@ -46,14 +48,16 @@ public class BoardWriteServlet extends HttpServlet {
         HttpSession session = req.getSession();
         String writer = (String) session.getAttribute("login");
 
-        //로그인된 사용자라면 글 제출 가능
-        if(writer != null) {
+        try {
+            //로그인된 사용자라면 글 제출 가능
+            if (writer == null)
+                throw new AuthenticatedException("글을 작성할 권한이 없습니다. 로그인 해주세요.");
+
             Board board = new Board(title, content, writer);
             boardService.write(board);
             alertAndRedirect(req, "글이 작성되었습니다.", req.getContextPath() + "/boards", resp);
-            req.getRequestDispatcher(ROOT + "alert.jsp").forward(req, resp);
-        } else {
-            alertAndRedirect(req, "접근할 수 없습니다. 글을 작성하려면 로그인을 해주세요", req.getContextPath(), resp);
+        } catch (AuthenticatedException e) {
+            alertAndRedirect(req, e.getMessage(), req.getContextPath(), resp);
         }
     }
 }
