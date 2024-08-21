53b2f45f3c42b164145840f20943bdb4e787191c
jhk01007 <jhk001007@naver.com>
2024-08-21 21:59:48 +0900

    feat: MemberController.java, LoginDTO.java, SignUpDTO.java
    회원 컨트롤러 기능 추가

--- src/main/java/org/example/board/mvc/controller/MemberController.java
+++ src/main/java/org/example/board/mvc/controller/MemberController.java
@@ -1,101 +1,103 @@
 package org.example.board.mvc.controller;
 
-
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import org.example.board.domain.Member;
+import org.example.board.dto.LoginDTO;
+import org.example.board.dto.SignUpDTO;
 import org.example.board.exception.IdDuplicateException;
-import org.example.board.exception.LoginException;
-import org.example.board.mvc.controller.util.Alert;
 import org.example.board.mvc.service.MemberService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Optional;
 
-import static org.example.board.mvc.controller.util.Alert.setAlertInfo;
+import static org.example.board.util.Alert.setAlertInfo;
+import static org.example.board.util.AppContext.CONTEXT_PATH;
 
-public class MemberController implements MyController{
+@Controller
+@RequestMapping("/members")
+public class MemberController {
 
-    MemberService memberService = MemberService.getInstance();
-    @Override
-    public String process(HttpServletRequest req, HttpServletResponse resp) {
+    private MemberService memberService;
 
-        String action = Optional.ofNullable(req.getPathInfo()).orElse("");
-        System.out.println(action);
-        String viewName = "";
-
-        if(action.equals("/signup")) {
-            if(req.getMethod().equals("GET"))
-                viewName = getSignUpForm(); // GET: 회원가입 폼 요청
-            else if(req.getMethod().equals("POST"))
-                viewName = signup(req); // POST: 회원가입 요청
-
-        } else if(action.equals("/login")) {
-            if(req.getMethod().equals("GET"))
-                viewName = getLoginForm(); // GET: 로그인 폼 요청
-            else if(req.getMethod().equals("POST"))
-                viewName = login(req, resp); // POST: 회원가입 요청
-        } else if (action.equals("/logout")) {
-            viewName = logout(req); // POST: 로그아웃 요청
-        }
-        return viewName;
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
     }
 
-    private String getSignUpForm() {
+    @GetMapping("/signup")
+    public String signupForm() {
         return "member/signup_form";
     }
 
-    private String signup(HttpServletRequest req) {
-        Member member = new Member(req.getParameter("memberid"), req.getParameter("password"));
+    @PostMapping("/signup")
+    public ModelAndView signup(@ModelAttribute SignUpDTO signUpDTO) {
+        Member member = new Member(signUpDTO.getMemberid(), signUpDTO.getPassword());
+        ModelAndView modelAndView = new ModelAndView("alert");
+
+
         try {
             memberService.signUp(member);
-            setAlertInfo(req, "회원가입에 성공했습니다. 환영합니다.", req.getContextPath() + "/main");
+            setAlertInfo(modelAndView, "회원가입에 성공했습니다. 환영합니다.", CONTEXT_PATH + "/main");
         } catch (IdDuplicateException e) {
-            setAlertInfo(req, e.getMessage(), req.getContextPath() + "/members/signup");
+            setAlertInfo(modelAndView, e.getMessage(), CONTEXT_PATH + "/members/signup");
         }
-        return "alert";
+
+        return modelAndView;
     }
 
-    /**
-     * [GET] /members/login: 로그인 폼 요청
-     * @return
-     */
-    private String getLoginForm() {
+    @GetMapping("/login")
+    public String loginForm() {
         return "member/login_form";
     }
 
-    /**
-     * [POST] /members/login: 로그인 요청
-     * @param req
-     * @param resp
-     * @return
-     */
-    private String login(HttpServletRequest req, HttpServletResponse resp) {
-        String memberId = req.getParameter("memberid");
-        String password = req.getParameter("password");
-        String rememberMe = req.getParameter("rememberMe");
+    @PostMapping("/login")
+    public ModelAndView login(@ModelAttribute LoginDTO loginDTO, HttpServletRequest req, HttpServletResponse resp) {
+        String memberId = loginDTO.getMemberId();
+        String password = loginDTO.getPassword();
+        String rememberMe = loginDTO.getRememberMe();
+        ModelAndView modelAndView = new ModelAndView("alert");
 
         try {
             memberService.login(memberId, password);
             HttpSession session = req.getSession();
             session.setAttribute("login", memberId);
+
             // 아이디 기억하기 체크 시 쿠키 추가
-            if (rememberMe != null)
+            if(rememberMe != null) {
                 resp.addCookie(getRememberIdCookie(memberId));
-                // 체크 해제시 기존의 저장된 쿠키 무효화
-            else {
+            }
+            // 체크 해제시 기존의 저장된 쿠키 무효화
+            else  {
                 Optional<Cookie> invalidatedCookie = Optional.ofNullable(invalidateRememberIdCookie(req.getCookies()));
                 invalidatedCookie.ifPresent(resp::addCookie);
             }
-
-            setAlertInfo(req, memberId + "님 환영합니다.", req.getContextPath() + "/main");
-        } catch (LoginException e) {
-            setAlertInfo(req, e.getMessage(), req.getContextPath() + "/members/login");
+            setAlertInfo(modelAndView, memberId + "님 환영합니다.", CONTEXT_PATH + "/main");
+        } catch (Exception e) {
+            setAlertInfo(modelAndView, e.getMessage(), CONTEXT_PATH + "/members/login");
         }
 
-        return "alert";
+        return modelAndView;
     }
+
+    @PostMapping("/logout")
+    public ModelAndView logout(HttpSession session) {
+        ModelAndView modelAndView = new ModelAndView("alert");
+        session.invalidate();
+        setAlertInfo(modelAndView, "로그아웃 되었습니다", CONTEXT_PATH);
+
+        return modelAndView;
+    }
+
+
+
 
     private static Cookie getRememberIdCookie(String memberId) {
         Cookie cookie = new Cookie("rememberId", memberId);
@@ -114,19 +116,5 @@ public class MemberController implements MyController{
         }
 
         return null;
-    }
-
-    /**
-     * [POST]/members/logout: 로그아웃 요청
-     * @param req
-     * @return
-     */
-    private String logout(HttpServletRequest req) {
-        HttpSession session = req.getSession();
-        session.invalidate();
-
-        setAlertInfo(req, "로그아웃 되었습니다.", req.getContextPath());
-
-        return "alert";
     }
 }
