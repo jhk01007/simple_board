4b7a60dfafd2c13fe3767b2936b2f00452bd8b13
jhk01007 <jhk001007@naver.com>
2024-08-21 21:17:19 +0900

    feat: Alert 클래스: alert에 띄울 메시지와 alert 후 redirect할 페이지 경로 지정

--- src/main/java/org/example/board/util/Alert.java
+++ src/main/java/org/example/board/util/Alert.java
@@ -1,12 +1,6 @@
-package org.example.board.mvc.controller.util;
+package org.example.board.util;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-
-import java.io.IOException;
-
-import static org.example.board.constant.ViewsPath.ROOT;
 
 
 public class Alert {
