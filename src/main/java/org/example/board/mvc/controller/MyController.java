02f9788ee412f97d0622a908d4046cbe81a1f09d
jhk01007 <jhk001007@naver.com>
2024-08-21 21:47:57 +0900

    remove: ControllerMapper.java, FrontController.java, BoardController.java, MyController.java
    이전 프로젝트에서 사용하던 컨트롤러 들 삭제

--- src/main/java/org/example/board/mvc/controller/MyController.java
+++
@@ -1,9 +0,0 @@
-package org.example.board.mvc.controller;
-
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-
-public interface MyController {
-
-    String process(HttpServletRequest req, HttpServletResponse resp);
-}
