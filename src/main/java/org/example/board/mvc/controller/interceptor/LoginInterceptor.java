939cea9abef23cb83729626150fc0c7b91f1a581
jhk01007 <jhk001007@naver.com>
2024-08-27 00:50:12 +0900

    feat: 페이지네이션 기능 추가

---
+++ src/main/java/org/example/board/mvc/controller/interceptor/LoginInterceptor.java
@@ -0,0 +1,24 @@
+package org.example.board.mvc.controller.interceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import static org.example.board.util.Alert.setAlertInfo;
+
+@Component
+public class LoginInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String loginId = (String) request.getSession().getAttribute("login");
+
+        if(loginId == null) {
+            setAlertInfo(request, "비회원은 접근할 수 없습니다. 로그인 해주세요.", request.getContextPath()+"/members/login");
+            request.getRequestDispatcher("/WEB-INF/views/alert.jsp").forward(request, response);
+            return false;
+        }
+
+        return true;
+    }
+}
