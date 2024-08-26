b2232b1f9793cf002214d85ad5302df51ab18ac5
jhk01007 <jhk001007@naver.com>
2024-08-26 22:05:19 +0900

    Delete ver4

--- very-simple-board_ver4/src/main/java/org/example/board/constant/ViewsPath.java
+++
@@ -1,6 +0,0 @@
-package org.example.board.constant;
-
-public class ViewsPath {
-
-    public static final String ROOT = "/WEB-INF/views/";
-}
--- very-simple-board_ver4/src/main/java/org/example/board/util/AppContext.java
+++
@@ -1,6 +0,0 @@
-package org.example.board.util;
-
-public class AppContext {
-
-    public static final String CONTEXT_PATH = "/oob";
-}
